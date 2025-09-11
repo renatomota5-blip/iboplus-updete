eed3c3e39ebda968daf9a1872a8bf3184a3e9570
renatomota5-blip <renatomota5@gmail.com>
2025-09-11 11:53:07 -0300

    Create Constants.kt

---
+++ app/src/main/java/tech/jrplay/iboplus/ui/Constants.kt
@@ -0,0 +1,13 @@
+package tech.jrplay.iboplus.ui
+object Constants {
+  const val BASE = "https://iboplus.motanetplay.top"
+  const val SETTING = "$BASE/setting.php"
+  const val ADS1 = "$BASE/apiautoads.php"
+  const val ADS2 = "$BASE/apiautoads2.php"
+  const val LOGO = "$BASE/logo.php"
+  const val BG = "$BASE/bg.php"
+  const val QR = "$BASE/qr.php"
+  const val CONTATO = "$BASE/contato.php"
+  const val VENCIMENTO = "$BASE/getappuser.php"
+  const val PLAYLIST = "$BASE/playlist.php"
+}
